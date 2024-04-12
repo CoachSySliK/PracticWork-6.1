2c58ab8dbd476fa25142d07f3b39ad39366c62d2
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-12 18:57:33 +0300

    Добавьте файлы проекта.

---
+++ PracticWork-6.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int hour = 0;
+    
+    hour:
+    cout << "12-ти часовой формат времени\n";
+    cout << "Который час? ";
+    cin >> hour;      
+     
+    if (hour < 1 || hour > 12) {
+        cout << "Так не бывает!\n";
+        goto hour;
+    }
+    
+    
+    int i = 0;
+    while (i < hour) {
+        cout << "Ку-ку!\n";
+        i++;
+    }
+
+    return 0;
+}
